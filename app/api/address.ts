b8293a96d1c93ab49b9dc3dfad673d204328873f
Chad <98scrapyg@gmail.com>
2023-03-26 10:39:07 -0500

    geopify config file

--- app/api/address.ts
+++ app/api/address.ts
@@ -1,8 +1,9 @@
 import { LatLng } from "react-native-maps";
+import config from "../config/api";
 
 const getAddress = async (coordinates: LatLng) => {
 	const endpoint = "https://api.geoapify.com/v1/geocode/reverse?";
-	const key = "310e3ad74eb24148a720b9860f02f35a";
+	const key = config.geoapifyKey;
 
 	const result = await fetch(
 		`${endpoint}lat=${coordinates.latitude}&lon=${coordinates.longitude}&apiKey=${key}`
---
+++ app/config/api.tsx
@@ -0,0 +1,3 @@
+export default {
+    geoapifyKey: "YOUR GEOAPIFY KEY HERE",
+}
